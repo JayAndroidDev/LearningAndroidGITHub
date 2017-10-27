f0028b21ddfdbf84414ced92effe3df0c50ab0b7
JAYAPRAKASH MEYYAPPAN <jayaprakash_feb6@yahoo.com>
2017-10-27 15:46:00 +0530

    This is my first commit to GIT.

--- app/src/main/java/com/example/androidlearning/github/ItemDetailActivity.java
+++ app/src/main/java/com/example/androidlearning/github/ItemDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
@@ -35,6 +36,7 @@ public class ItemDetailActivity extends AppCompatActivity {
             }
         });
 
+        Log.i("Tag","Testing GIT");
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
